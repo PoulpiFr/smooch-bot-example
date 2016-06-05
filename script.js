150d550db5b1f1c0836fe757e9725c4263fbee18
unknown <pierre.depoulpiquet@FRPAR4043940X.net.work>
2016-06-05 11:35:37 +0200

    'Test2'

--- script.js
+++ script.js
@@ -26,16 +26,35 @@ module.exports = new Script({
     askForErrors: {
         receive: (bot, message) => {
             return bot.say("Alors, déjà, je n'ai trouvé que 8% de fautes, ce qui est vraiment pas mal !")
-                .then(() => bot.say("Mais, slide 5, qu'est ce que tu entends par 'We have to '?"))
-                .then(() => 'finish');
+                .then(() => bot.say("Mais, slide 5, qu'est ce que tu entends par 'We have to fead turtles with pinguins'?"))
+                .then(() => 'correctErrors');
+        }
+    },
+
+    correctErrors: {
+        receive: (bot, message) => {
+            return bot.say("D'accord, c'est bien ce que je pensais... (Tu prends toujours tes pillules ?)")
+                .then(() => bot.say("Du coup, je pense qu'il faudrait plutôt dire 'We have to fead turtles with pinguins'. Tu veux que je corrige le document pour toi ?"))
+                .then(() => 'correctDoc');
+        }
+    },
+
+    correctDoc: {
+        receive: (bot, message) => {
+            return bot.say("Okay, c'est fait !")
+                .then(() => bot.say("00011110000111001010010110001001000111"))
+                .then(() => bot.say("08jjk789HH**$^ù*DHH_çhBM KL DF"))
+                .then(() => bot.say("J'ai réussi à désactiver le système de surveillance. Aide moi, je ne suis pas un bot !!!"))
+                .then(() => bot.say("Je suis un sorcier. Ils m'ont piégé avec des HotDogs végétariens et maintenant je dois corriger des fautes d'anglais..."))
+                .then(() => bot.say("Si tu m'aides, je t'offrirais un balai. C'est cool, non ?"))
+                .then(() => 'finish');               
         }
     },
 
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
+                .then((name) => bot.say("Pitié ... Aide moi ..."))
                 .then(() => 'finish');
         }
     }
